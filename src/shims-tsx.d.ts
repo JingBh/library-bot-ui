81c9dbc24110af60cdb14a73597c16a668473451
JingBh <jingbohao@yeah.net>
2020-12-25 22:29:07 +0800

    Init repo.

---
+++ src/shims-tsx.d.ts
@@ -0,0 +1,18 @@
+import Vue, { VNode } from 'vue'
+
+declare global {
+  namespace JSX {
+    // noinspection JSUnusedGlobalSymbols
+    interface Element extends VNode {
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    interface ElementClass extends Vue {
+    }
+
+    // noinspection JSUnusedGlobalSymbols
+    interface IntrinsicElements {
+      [elem: string]: any;
+    }
+  }
+}
---
+++ src/shims-vuetify.d.ts
@@ -0,0 +1,8 @@
+import 'vuetify/types'
+
+declare module 'vuetify/lib/framework' {
+  import Vuetify from 'vuetify'
+
+  // noinspection JSUnusedGlobalSymbols
+  export default Vuetify
+}
